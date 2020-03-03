897cf9792f3a1db321cbf2f2967275a1e2f733f0
Dan Orban <dtorban@umn.edu>
2020-03-03 10:28:39 -0600

    Adding plugininfo.h

---
+++ include/openspace/util/plugininfo.h
@@ -0,0 +1,16 @@
+#ifndef __OPENSPACE_CORE___PLUGININFO___H__
+#define __OPENSPACE_CORE___PLUGININFO___H__
+
+#include <string>
+#include <openspace/util/factorymanager.h>
+
+namespace openspace {
+
+struct PluginInfo {
+	FactoryManager* factoryManager;
+	std::string path;
+};
+
+}
+
+#endif
